a2fe7b229db6d2017e7060e803b3a1533c75812e
Jorge Francisco Aguirre Concha <jorgef.aguir@gmail.com>
2024-09-12 22:25:22 -0300

    finished intro and responsive, added icons, finishing Modal/cards

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import("tailwindcss").Config} */
+const colors = require('tailwindcss/colors')
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	darkMode: "class",
 	theme: {
+		colors: {
+			crimson: "#751717",
+			amber: colors.amber,
+			red: colors.red,
+			black: colors.black,
+			transparent: "transparent",
+		},
 		extend: {},
 	},
 	plugins: [],
