08b01899ad100fcb41f2c49a3831f656c1532533
Jorge Francisco Aguirre Concha <jorgef.aguir@gmail.com>
2024-09-18 14:17:37 -0300

    started working on portfolio section

---
+++ src/data/proyects.js
@@ -0,0 +1,74 @@
+export const proyectsData = [
+	{
+		title: "Desarrollador Full-stack",
+		description: "Desarrollo de aplicaciones web y móviles",
+		company: "Freelance",
+		images: [
+			"https://via.placeholder.com/150",
+			"https://via.placeholder.com/150"
+		],
+		link: "https://www.google.com",
+		stack: ["React", "Node", "MongoDB"]
+
+	},
+	{
+		title: "Desarrollador Front-end",
+		description: "Desarrollo de aplicaciones web y móviles",
+		company: "Freelance",
+		images: [
+			"https://via.placeholder.com/150",
+			"https://via.placeholder.com/150"
+		],
+		link: "https://www.google.com",
+		stack: ["React", "Node", "MongoDB"]
+
+	},
+	{
+		title: "Desarrollador Back-end",
+		description: "Desarrollo de aplicaciones web y móviles",
+		company: "Freelance",
+		images: [
+			"https://via.placeholder.com/150",
+			"https://via.placeholder.com/150"
+		],
+		link: "https://www.google.com",
+		stack: ["React", "Node", "MongoDB"]
+
+	},
+	{
+		title: "Desarrollador Full-stack",
+		description: "Desarrollo de aplicaciones web y móviles",
+		company: "Freelance",
+		images: [
+			"https://via.placeholder.com/150",
+			"https://via.placeholder.com/150"
+		],
+		link: "https://www.google.com",
+		stack: ["React", "Node", "MongoDB"]
+
+	},
+	{
+		title: "Desarrollador Front-end",
+		description: "Desarrollo de aplicaciones web y móviles",
+		company: "Freelance",
+		images: [
+			"https://via.placeholder.com/150",
+			"https://via.placeholder.com/150"
+		],
+		link: "https://www.google.com",
+		stack: ["React", "Node", "MongoDB"]
+
+	},
+	{
+		title: "Desarrollador Back-end",
+		description: "Desarrollo de aplicaciones web y móviles",
+		company: "Freelance",
+		images: [
+			"https://via.placeholder.com/150",
+			"https://via.placeholder.com/150"
+		],
+		link: "https://www.google.com",
+		stack: ["React", "Node", "MongoDB"]
+
+	}
+]
