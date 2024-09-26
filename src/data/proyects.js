c91235716745a5c2d834938497b1bf71d5084f79
Jorge Francisco Aguirre Concha <jorgef.aguir@gmail.com>
2024-09-26 20:30:04 -0300

    placeholder for proyects

--- src/data/proyects.js
+++ src/data/proyects.js
@@ -1,74 +1,112 @@
-export const proyectsData = [
-	{
-		title: "Desarrollador Full-stack",
-		description: "Desarrollo de aplicaciones web y móviles",
-		company: "Freelance",
-		images: [
-			"https://via.placeholder.com/150",
-			"https://via.placeholder.com/150"
+export const proyectsData = {
+	es: {
+		title: 'Proyectos',
+		wip: 'Estoy trabajando en esta sección, perdón las molestias 😋',
+		showcase: [
+			{
+				title: "Desarrollador Full-stack",
+				description: "Desarrollo de aplicaciones web y móviles",
+				company: "Freelance",
+				images: [
+					"https://via.placeholder.com/150",
+					"https://via.placeholder.com/150"
+				],
+				link: "https://www.google.com",
+				stack: ["React", "Node", "MongoDB"]
+		
+			},
+			{
+				title: "Desarrollador Front-end",
+				description: "Desarrollo de aplicaciones web y móviles",
+				company: "Freelance",
+				images: [
+					"https://via.placeholder.com/150",
+					"https://via.placeholder.com/150"
+				],
+				link: "https://www.google.com",
+				stack: ["React", "Node", "MongoDB"]
+		
+			},
+			{
+				title: "Desarrollador Back-end",
+				description: "Desarrollo de aplicaciones web y móviles",
+				company: "Freelance",
+				images: [
+					"https://via.placeholder.com/150",
+					"https://via.placeholder.com/150"
+				],
+				link: "https://www.google.com",
+				stack: ["React", "Node", "MongoDB"]
+		
+			},
+			{
+				title: "Desarrollador Full-stack",
+				description: "Desarrollo de aplicaciones web y móviles",
+				company: "Freelance",
+				images: [
+					"https://via.placeholder.com/150",
+					"https://via.placeholder.com/150"
+				],
+				link: "https://www.google.com",
+				stack: ["React", "Node", "MongoDB"]
+		
+			},
 		],
-		link: "https://www.google.com",
-		stack: ["React", "Node", "MongoDB"]
-
+		other: [
+			{
+				title: "Desarrollador Full-stack",
+				description: "Desarrollo de aplicaciones web y móviles",
+				company: "Freelance",
+				images: [
+					"https://via.placeholder.com/150",
+					"https://via.placeholder.com/150"
+				],
+				link: "https://www.google.com",
+				stack: ["React", "Node", "MongoDB"]
+		
+			},
+			{
+				title: "Desarrollador Front-end",
+				description: "Desarrollo de aplicaciones web y móviles",
+				company: "Freelance",
+				images: [
+					"https://via.placeholder.com/150",
+					"https://via.placeholder.com/150"
+				],
+				link: "https://www.google.com",
+				stack: ["React", "Node", "MongoDB"]
+		
+			},
+			{
+				title: "Desarrollador Back-end",
+				description: "Desarrollo de aplicaciones web y móviles",
+				company: "Freelance",
+				images: [
+					"https://via.placeholder.com/150",
+					"https://via.placeholder.com/150"
+				],
+				link: "https://www.google.com",
+				stack: ["React", "Node", "MongoDB"]
+		
+			},
+			{
+				title: "Desarrollador Full-stack",
+				description: "Desarrollo de aplicaciones web y móviles",
+				company: "Freelance",
+				images: [
+					"https://via.placeholder.com/150",
+					"https://via.placeholder.com/150"
+				],
+				link: "https://www.google.com",
+				stack: ["React", "Node", "MongoDB"]
+		
+			},
+		]
 	},
-	{
-		title: "Desarrollador Front-end",
-		description: "Desarrollo de aplicaciones web y móviles",
-		company: "Freelance",
-		images: [
-			"https://via.placeholder.com/150",
-			"https://via.placeholder.com/150"
-		],
-		link: "https://www.google.com",
-		stack: ["React", "Node", "MongoDB"]
-
-	},
-	{
-		title: "Desarrollador Back-end",
-		description: "Desarrollo de aplicaciones web y móviles",
-		company: "Freelance",
-		images: [
-			"https://via.placeholder.com/150",
-			"https://via.placeholder.com/150"
-		],
-		link: "https://www.google.com",
-		stack: ["React", "Node", "MongoDB"]
-
-	},
-	{
-		title: "Desarrollador Full-stack",
-		description: "Desarrollo de aplicaciones web y móviles",
-		company: "Freelance",
-		images: [
-			"https://via.placeholder.com/150",
-			"https://via.placeholder.com/150"
-		],
-		link: "https://www.google.com",
-		stack: ["React", "Node", "MongoDB"]
-
-	},
-	{
-		title: "Desarrollador Front-end",
-		description: "Desarrollo de aplicaciones web y móviles",
-		company: "Freelance",
-		images: [
-			"https://via.placeholder.com/150",
-			"https://via.placeholder.com/150"
-		],
-		link: "https://www.google.com",
-		stack: ["React", "Node", "MongoDB"]
-
-	},
-	{
-		title: "Desarrollador Back-end",
-		description: "Desarrollo de aplicaciones web y móviles",
-		company: "Freelance",
-		images: [
-			"https://via.placeholder.com/150",
-			"https://via.placeholder.com/150"
-		],
-		link: "https://www.google.com",
-		stack: ["React", "Node", "MongoDB"]
-
+	en: {
+		title: 'Proyects',
+		wip: 'I am working on this section, sorry for the inconvenience 😋',
+		showcase: [],
+		other: []
 	}
-]
+}
