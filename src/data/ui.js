4eb00c440fcadfe870e0940ceb36ce3fdc281e62
Jorge Francisco Aguirre Concha <jorgef.aguir@gmail.com>
2024-09-26 13:19:13 -0300

    Added more english texts

---
+++ src/data/ui.js
@@ -0,0 +1,23 @@
+export const navData = {
+	es: {
+		home: 'Inicio',
+		exp: 'Experiencia',
+		projects: 'Proyectos',
+		contact: 'Contacto',
+	},
+	en: {
+		home: 'Home',
+		exp: 'Experience',
+		projects: 'Projects',
+		contact: 'Contact',
+	}
+}
+
+export const footerData = {
+	es: {
+		credits: 'Hecho con ❤️ por Atre',
+	},
+	en: {
+		credits: 'Made with	❤️ by Atre',
+	}
+}
