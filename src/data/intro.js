e4c1e3df50cbb651f362f5ed1d39ad47eca87209
Jorge Francisco Aguirre Concha <jorgef.aguir@gmail.com>
2024-09-26 11:34:31 -0300

    started to work on contact section and language switcher

---
+++ src/data/intro.js
@@ -0,0 +1,10 @@
+export const introPageData = {
+	es: {
+		cta: 'Disponible para trabajar',
+		intro: 'Hola, me dicen Atre, soy un Desarrollador Full-Stack🔥 con experiencia <span class="text-amber-200/90">6 años de experiencia</span> en diversas industrias. Busco generar <span class="text-amber-100 font-bold">soluciones creativas</span> para puzzles complejos. Me gusta mas el <span class="bg-gradient-to-br from-red-500 to-amber-500 font-bold bg-clip-text text-transparent">Front-End</span> 🚀, pero también disfruto de modelar DBs o construir APIs 🤓.',
+		cta2: 'En mi tiempo libre...',
+	},
+	en: {
+
+	}
+}
