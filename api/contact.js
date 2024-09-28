4ee53aed9c3c5d00de7553d857d2f113f03af071
Jorge Francisco Aguirre Concha <jorgef.aguir@gmail.com>
2024-09-28 12:31:15 -0300

    function was not actually returning, im sleepy

--- api/contact.js
+++ api/contact.js
@@ -1,18 +1,22 @@
 export default async function contact(req, res) {
 	const { method } = req;
+	const messages = {
+		missingFields: {
+			es: "Por favor, rellena todos los campos",
+			en: "Please fill all fields",
+		},
+		wrongMethod: {
+			es: "Método no permitido",
+			en: "Method not allowed",
+		},
+		success: {
+			es: "Mensaje enviado correctamente",
+			en: "Message sent successfully",
+		},
+	};
 	if (method === "POST") {
 		const { name, email, message, lang } = req.body; // check if i can make lang come from the headers
 		const defaultLang = lang || "es";
-		const messages = {
-			missingFields: {
-				es: "Por favor, rellena todos los campos",
-				en: "Please fill all fields",
-			},
-			success: {
-				es: "Mensaje enviado correctamente",
-				en: "Message sent successfully",
-			},
-		};
 		
 		if ( !name || !email || !message ) {
 			return res.status(406).json({ error: messages.missingFields[defaultLang] });
@@ -21,6 +25,12 @@ export default async function contact(req, res) {
 		console.log("Name: ", name);
 		console.log("Email: ", email);
 		console.log("Message: ", message);
+		console.log("Lang: ", lang);
 
-	}
+		return res.status(200).json({ status: "success", message: messages.success[defaultLang] });
+	} 
+	
+	res.setHeader("Allow", ["POST"]);
+	return res.status(405).json({ error: messages.wrongMethod[defaultLang] });
+	
 }
